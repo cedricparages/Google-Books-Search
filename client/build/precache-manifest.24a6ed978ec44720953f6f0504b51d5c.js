d518a014fea07a2ea9da8cca1c254d1032a41f77
CedricParages <cedricparages@gmail.com>
2019-10-15 18:04:58 -0400

    heroku edit

---
+++ client/build/precache-manifest.24a6ed978ec44720953f6f0504b51d5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7ba9d3971a4f6a5bc4ea",
+    "url": "/static/js/main.75ab2d53.chunk.js"
+  },
+  {
+    "revision": "417cb6d89b04eaff7faa",
+    "url": "/static/js/2.f5ee2e6d.chunk.js"
+  },
+  {
+    "revision": "7ba9d3971a4f6a5bc4ea",
+    "url": "/static/css/main.be9cd952.chunk.css"
+  },
+  {
+    "revision": "bdd5a94566f04a5b3cd4b9e7317c6644",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const apiRoutes = require("./routes/api-routes");
 // middleware 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
+// heroku code
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "production") {
 app.use("/api", apiRoutes);
 
 // MongoDB
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://cedd:ironmaiden78@ds229078.mlab.com:29078/heroku_klm3vcvd";
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
@@ -27,7 +27,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://cedd:ironmaiden78@ds229078.mlab.com:29078/heroku_klm3vcvd");
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
